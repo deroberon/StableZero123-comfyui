9b2e7fb05712b9ec7405402663054c7c93263fe6
deroberon <andre.andrade@gmail.com>
2023-12-17 21:55:17 -0800

    Implementing the usage of controlnet, but it seems that it is not working. Also correcting the issue in the SDZero ImageSplit node. Now it returns all the images.

--- stablezero123.py
+++ stablezero123.py
@@ -1,7 +1,7 @@
 import torch
 import requests
 from PIL import Image
-from diffusers import DiffusionPipeline, EulerAncestralDiscreteScheduler
+from diffusers import DiffusionPipeline, EulerAncestralDiscreteScheduler, ControlNetModel
 import numpy as np
 
 #tiles = [im[x:x+M,y:y+N] for x in range(0,im.shape[0],M) for y in range(0,im.shape[1],N)]
@@ -58,7 +58,7 @@ class ImageSplit:
             t_tiles.append(t_tile)
 
         s=t_tiles[0]
-        for i in range(1,len(t_tiles)-1):
+        for i in range(1,len(t_tiles)):
             s = torch.cat((s, t_tiles[i]), dim=0)
 
         return (s,)
@@ -116,15 +116,80 @@ class Stablezero123:
 
         return (image,)
 
+class Stablezero123WithDepth:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "images": ("IMAGE", ),
+                "depth_images": ("IMAGE", ),
+                "ckpt_name": ("STRING", {
+                    "multiline": False,
+                    "default": "sudo-ai/zero123plus-v1.1"
+                }),
+                "control_model_name": ("STRING", {
+                    "multiline": False,
+                    "default": "sudo-ai/controlnet-zp11-depth-v1"
+                }),
+                "pipeline_name": ("STRING", {
+                    "multiline": False,
+                    "default": "sudo-ai/zero123plus-pipeline"
+                }),
+                "inference_steps": ("INT", {
+                    "default": 75, 
+                    "min": 1, #Minimum value
+                    "max": 100, #Maximum value
+                    "step": 1, #Slider's step
+                    "display": "number" 
+                }),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "execute"
+    CATEGORY = "tests"
+
+    def execute(self, images, depth_images, ckpt_name, control_model_name, pipeline_name, inference_steps):
+        pipeline = DiffusionPipeline.from_pretrained(ckpt_name, custom_pipeline=pipeline_name, torch_dtype=torch.float16)
+        pipeline.add_controlnet(ControlNetModel.from_pretrained(control_model_name, torch_dtype=torch.float16), conditioning_scale=0.75)
+        
+        pipeline.scheduler = EulerAncestralDiscreteScheduler.from_config(
+        pipeline.scheduler.config, timestep_spacing='trailing')
+        pipeline.to('cuda:0')
+        
+        image=images[0]
+        i = 255. * image.cpu().numpy()
+        cond = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+        depth=depth_images[0]
+        ii = 255. * depth.cpu().numpy()
+        depth = Image.fromarray(np.clip(ii, 0, 255).astype(np.uint8))
+
+        ##cond = Image.open(requests.get("https://d.skis.ltd/nrp/sample-data/lysol.png", stream=True).raw)
+        ## Converter para formato img
+
+        image = pipeline(cond, depth_image=depth, num_inference_steps=inference_steps).images[0]
+        
+        image = image.convert("RGB")
+        image = np.array(image).astype(np.float32) / 255.0
+        image = torch.from_numpy(image)[None,]
+
+        return (image,)
+
+
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
+    #"Stablezero123WithDepth" : Stablezero123WithDepth,
     "Stablezero123": Stablezero123,
     "SDZero ImageSplit" : ImageSplit
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
+    #"Stablezero123WithDepth": "Stablezero123WithDepth",
     "Stablezero123": "Stablezero123",
     "SDZero ImageSplit" : "SDZero ImageSplit"
 }
